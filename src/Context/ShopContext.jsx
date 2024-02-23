0dbef616346185e996e8e4ed7af7c6f71dac489e
Davidumhazavani <mybizness2017@gmail.com>
2024-02-23 20:40:12 +0100

    E=Commerce Website (React js)

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import all_product from '../Components/assets/all_product';
+
+
+export const ShopContext = createContext(undefined);
+
+const ShopContextProvider = (props) => {
+
+    const contextValue = {all_product};
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
